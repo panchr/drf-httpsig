6c711b8177dd0813d68d682a55f3fc60adaf4368
Adam Knight <adam@movq.us>
2014-07-01 16:33:09 -0500

    Removed models.py -- user should specify and manage this.

--- drf_httpsig/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Token(models.Model):
-    user   = models.ForeignKey(User)
-    keyId  = models.CharField(max_length=64)
-    secret = models.CharField(max_length=64)

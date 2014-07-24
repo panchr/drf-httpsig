fc708d030b200f1f1dad811be0ed6389a8f1d751
Adam Knight <adam@movq.us>
2014-07-24 11:53:35 -0500

    Updated requirements.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     install_requires=[
         'django>=1.6,<1.7',
         'djangorestframework>=2.3,<2.4',
-        'httpsig>=1.0.2,<2.0'
+        'httpsig>=1.0.2,<1.1'
     ],
     tests_require=['tox'],
 )
